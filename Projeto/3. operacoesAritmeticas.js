9fd1be91130f56c6914dc8a5823f2039789ada13
Renan Lemos <renanlemos1612@gmail.com>
2022-04-24 00:51:23 -0400

    Update

--- Projeto/3. operacoesAritmeticas.js
+++ Projeto/3. operacoesAritmeticas.js
@@ -1,3 +1,7 @@
 console.log("Operações Aritméticas");
 
 console.log(10 + (8 * 2));
+console.log(2 + 2);
+console.log((10 + 8) * 2);
+
+console.log("O Ano é " + 2022 + ("!"));
---
+++ Projeto/4. conversaoDeTipos.js
@@ -0,0 +1,11 @@
+console.log("Conversão de tipos");
+
+console.log("Ano " + 2022);
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt("2"));
+
+console.log("10" / "2");
+console.log("Renan" / "2"); // Erro retornado = NaN Not An Number
+
+console.log(6.5);
+console.log(6,5);
